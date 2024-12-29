4cbac785c552ff76292c202ec5af94798d759e40
1Manojkumar1 <24eg105p52@anurag.edu.in>
2024-12-29 12:40:25 +0530

    Project added

---
+++ src/App.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const App = () => {
+  const [todos, setTodos] = useState([]);
+  const [newTodo, setNewTodo] = useState('');
+
+  const addTodo = () => {
+    if (newTodo.trim()) {
+      setTodos([...todos, newTodo]);
+      setNewTodo('');
+    }
+  };
+
+  const removeTodo = (index) => {
+    const updatedTodos = todos.filter((_, i) => i !== index);
+    setTodos(updatedTodos);
+  };
+
+  return (
+    <div>
+      <h1>Todo List</h1>
+      <input
+        type="text"
+        value={newTodo}
+        onChange={(e) => setNewTodo(e.target.value)}
+        placeholder="Add a new todo"
+      />
+      <button onClick={addTodo}>Add Todo</button>
+      <ul>
+        {todos.map((todo, index) => (
+          <li key={index}>
+            {todo} <button onClick={() => removeTodo(index)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/reportWebVitals.js
@@ -0,0 +1,10 @@
+// Uncomment the following line if you want to track web vitals
+// import { reportWebVitals } from 'web-vitals';
+
+// Function to log the results or send to an analytics endpoint
+function reportWebVitals(metric) {
+    console.log(metric);
+  }
+  
+  export default reportWebVitals;
+  
